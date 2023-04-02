03b45d8d4bcbb0edecea1548a56182d2cd6e7069
ANGEL CAMARA RAMOS <103178330+2kAngel@users.noreply.github.com>
2023-04-03 00:41:49 +0200

    +model(3-basic)

--- videojuegos/common/src/main/java/es/upsa/dasi/videojuegos/model/Desarrolladora.java
+++ videojuegos/common/src/main/java/es/upsa/dasi/videojuegos/model/Desarrolladora.java
@@ -1,4 +1,24 @@
 package es.upsa.dasi.videojuegos.model;
 
-public record Desarrolladoras() {
-}
+import lombok.Builder;
+import lombok.With;
+
+import java.util.Date;
+
+@Builder(setterPrefix = "with")
+@With
+public record Desarrolladora(
+                            String id,
+                            String nombre,
+                            String nacionalidad,
+                            Date fecha_creacion,
+                            String foto
+                            )
+{ }
+/*
+*  ID VARCHAR(9),
+    NOMBRE VARCHAR(100),
+    NACIONALIDAD VARCHAR(100),
+    FECHA_CREACION DATE,
+    FOTO VARCHAR(200)
+* */
--- videojuegos/common/src/main/java/es/upsa/dasi/videojuegos/model/Plataforma.java
+++ videojuegos/common/src/main/java/es/upsa/dasi/videojuegos/model/Plataforma.java
@@ -1,4 +1,21 @@
 package es.upsa.dasi.videojuegos.model;
 
-public record Plataformas() {
-}
+
+import lombok.Builder;
+import lombok.With;
+
+import java.util.Date;
+
+@Builder(setterPrefix = "with")
+@With
+public record Plataforma(
+        String id,
+        String nombre,
+        Date fecha,
+        String foto
+) { }
+
+/* ID VARCHAR(9),
+    NOMBRE VARCHAR(100),
+    FECHA_LANZAMIENTO DATE,
+    FOTO VARCHAR(200), */
